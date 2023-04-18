44e117646b9832572c36e701cdb8dfb3ddde9f76
Shirkakaoo <mohammad98.master@gmail.com>
2023-04-18 11:57:50 +0330

    DependencyInjection Added

---
+++ Penfolio.Web/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿namespace Penfolio.Web;
+
+internal static class DependencyInjection
+{
+
+        internal static IServiceCollection InjectControllers(this IServiceCollection services) =>
+                services.AddControllers().Services;
+
+        internal static IServiceCollection InjectAddEndpointsApiExplorer(this IServiceCollection services) =>
+                services.AddEndpointsApiExplorer();
+
+        internal static IServiceCollection InjectSwaggerGen(this IServiceCollection services) =>
+                services.AddSwaggerGen();
+}
